7b912b4eada2c38b8e7cda032a8372eafd398f74
23122039 <122670158+trungkiet2005@users.noreply.github.com>
2026-04-04 12:29:24 +0700

    Add training configuration files for various datasets with updated model parameters

--- train/imdbb.py
+++ train/imdbb.py
@@ -5,14 +5,14 @@ from core.config import cfg, update_cfg
 from core.get_data import create_dataset
 from core.get_model import create_model
 from core.trainer import run_k_fold
-from core.model_utils.hyperbolic_dist import hyperbolic_dist
+from train.zinc import _compute_loss, _ema_update
 
 
-def train(train_loader, model, optimizer, evaluator, device, momentum_weight,sharp=None, criterion_type=0):
+def train(train_loader, model, optimizer, evaluator, device, momentum_weight, sharp=None, criterion_type=0):
     criterion = torch.nn.SmoothL1Loss(beta=0.5)
     step_losses, num_targets = [], []
     for data in train_loader:
-        if model.use_lap: # Sign flips for eigenvalue PEs
+        if model.use_lap:
             batch_pos_enc = data.lap_pos_enc
             sign_flip = torch.rand(batch_pos_enc.size(1))
             sign_flip[sign_flip >= 0.5] = 1.0
@@ -20,61 +20,32 @@ def train(train_loader, model, optimizer, evaluator, device, momentum_weight,sha
             data.lap_pos_enc = batch_pos_enc * sign_flip.unsqueeze(0)
         data = data.to(device)
         optimizer.zero_grad()
-        target_x, target_y = model(data)
-        if criterion_type == 0:
-            loss = criterion(target_x, target_y)
-        elif criterion_type == 1:
-            loss = F.mse_loss(target_x, target_y)
-        elif criterion_type == 2:
-            loss = hyperbolic_dist(target_x, target_y)
-        else:
-            print('Loss function not supported! Exiting!')
-            exit()
-
-        # Will need these for the weighted average at the end of the epoch
+        loss, num_t = _compute_loss(model, data, criterion, criterion_type)
         step_losses.append(loss.item())
-        num_targets.append(len(target_y))
-        
-        # Update weights of the network 
+        num_targets.append(num_t)
         loss.backward()
         optimizer.step()
-
-        # Other than the target encoder, here we use exponential smoothing
         with torch.no_grad():
-            for param_q, param_k in zip(model.context_encoder.parameters(), model.target_encoder.parameters()):
-                param_k.data.mul_(momentum_weight).add_((1.-momentum_weight) * param_q.detach().data)
-        
+            _ema_update(model, momentum_weight)
     epoch_loss = np.average(step_losses, weights=num_targets)
-    return None, epoch_loss # Leave none for now since maybe we'd like to return the embeddings for visualization
+    return None, epoch_loss
 
 
-@ torch.no_grad()
+@torch.no_grad()
 def test(loader, model, evaluator, device, criterion_type=0):
     criterion = torch.nn.SmoothL1Loss(beta=0.5)
     step_losses, num_targets = [], []
     for data in loader:
         data = data.to(device)
-        target_x, target_y = model(data)
-        if criterion_type == 0:
-            loss = criterion(target_x, target_y)
-        elif criterion_type == 1:
-            loss = F.mse_loss(target_x, target_y)
-        elif criterion_type == 2:
-            loss = hyperbolic_dist(target_x, target_y)
-        else:
-            print('Loss function not supported! Exiting!')
-            exit()
-
-        # Will need these for the weighted average at the end of the epoch
+        loss, num_t = _compute_loss(model, data, criterion, criterion_type)
         step_losses.append(loss.item())
-        num_targets.append(len(target_y))
-
+        num_targets.append(num_t)
     epoch_loss = np.average(step_losses, weights=num_targets)
     return None, epoch_loss
 
 
 if __name__ == '__main__':
     cfg.merge_from_file('train/configs/imdbb.yaml')
-    cfg = update_cfg(cfg) # we can specify the config file for the update here
+    cfg = update_cfg(cfg)
     cfg.k = 10
     run_k_fold(cfg, create_dataset, create_model, train, test)
